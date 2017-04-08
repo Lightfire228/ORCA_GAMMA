808516a92ded5c6b9a0f251c7ce716efb07e749f
ryandixon <dixon104@marshall.edu>
2017-04-07 21:27:07 -0400

    Fixed: datetime issue Changed: Forums no default sort to newest thread first Removed: sorting of topics on forums

--- Orca_Gamma/Controllers/ForumsController.cs
+++ Orca_Gamma/Controllers/ForumsController.cs
@@ -37,7 +37,7 @@ namespace Orca_Gamma.Controllers
         public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
             ViewBag.CurrentSort = sortOrder;
-            ViewBag.nameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.nameSortParm = String.IsNullOrEmpty(sortOrder) ? "date_desc" : "";
 
             if (searchString != null)
             {
@@ -60,7 +60,7 @@ namespace Orca_Gamma.Controllers
                     threads = threads.OrderByDescending(s => s.Subject);
                     break;
                 default:
-                    threads = threads.OrderBy(s => s.Subject);
+                    threads = threads.OrderByDescending(s => s.Date);
                     break;
             }
 
@@ -100,7 +100,7 @@ namespace Orca_Gamma.Controllers
                 Subject = model.Thread.Subject,
                 FirstPost = model.Thread.FirstPost,
                 IsDeleted = false,
-                Date = DateTime.Now
+                Date = DateTime.Now.ToLocalTime()
                 
             };
 
@@ -108,7 +108,7 @@ namespace Orca_Gamma.Controllers
             {
                 User = user,
                 Body = model.Body,
-                Date = DateTime.Now,
+                Date = DateTime.Now.ToLocalTime(),
                 Thread = post
             };
 
